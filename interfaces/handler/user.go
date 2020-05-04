db0284837241e908210f689a6e61eadc0dbb36d5
Sergio Martin <sca@Sergios-iMac.T-mobile.com>
2020-05-04 14:54:10 -0500

    saving progress

--- interfaces/handler/user.go
+++ interfaces/handler/user.go
@@ -2,8 +2,10 @@ package handler
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/smartinsantos/go-auth-api/domain/entitity"
 	"github.com/smartinsantos/go-auth-api/infrastructure/datastore"
 	"net/http"
+	"time"
 )
 
 // userController struct defines the dependencies that will be used
@@ -12,6 +14,18 @@ type UserHandler struct {
 }
 
 func (uc * UserHandler) Register (context *gin.Context) {
+	mockUser := entitity.User{
+		ID:           1,
+		EmailAddress: "mock@mock.com",
+		FirstName:    "mock",
+		LastName:     "mock",
+		Password:     "mockPassword",
+		RegisteredAt: time.Now(),
+		ModifiedAt:   time.Now(),
+	}
+
+	uc.ds.CreateUser(&mockUser)
+
 	context.String(http.StatusOK, "Register")
 }
 
