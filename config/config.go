52b55cbae3637012ddee9cf53c81576c7f560d63
Sergio Martin <sergio@onlinemeded.org>
2020-05-09 13:02:26 -0500

    saving progress

--- config/config.go
+++ config/config.go
@@ -1,49 +1,50 @@
 package config
 
 import (
-	"github.com/gobuffalo/packr"
-	"github.com/joho/godotenv"
 	"log"
 	"strconv"
 	"sync"
+
+	"github.com/gobuffalo/packr"
+	"github.com/joho/godotenv"
 )
 
 // App configuration
 type AppConfig struct {
-    Env		string
-    Debug	bool
-    Addr	string
+	Env   string
+	Debug bool
+	Addr  string
 }
 
 // Database configuration
 type DBConfig struct {
-	Driver 	string
-	Name	string
-	User	string
-	Pass	string
-	Host	string
-	Port	string
+	Driver string
+	Name   string
+	User   string
+	Pass   string
+	Host   string
+	Port   string
 }
 
 // Config Wrapper for all configurations
 type Config struct {
-    AppConfig AppConfig
-    DBConfig DBConfig
+	AppConfig AppConfig
+	DBConfig  DBConfig
 }
 
-var defaultConfig = &Config {
-	AppConfig: AppConfig {
-		Env: "local",
+var defaultConfig = &Config{
+	AppConfig: AppConfig{
+		Env:   "local",
 		Debug: true,
-		Addr: ":8080",
+		Addr:  ":8080",
 	},
-	DBConfig: DBConfig {
+	DBConfig: DBConfig{
 		Driver: "mysql",
-		Name: "db_name",
-		User: "db_user",
-		Pass: "db_password",
-		Host: "db_host",
-		Port: "db_port",
+		Name:   "db_name",
+		User:   "db_user",
+		Pass:   "db_password",
+		Host:   "db_host",
+		Port:   "db_port",
 	},
 }
 
@@ -52,31 +53,31 @@ var once sync.Once
 
 // Get returns Config instance
 func Get() *Config {
-	once.Do(func () {
+	once.Do(func() {
 		rootBox := packr.NewBox("../")
 		envStr, err := rootBox.FindString(".env")
 		if err != nil {
-			log.Println("Error: No .env file found")
+			log.Fatalln("Error: No .env file found")
 		}
 
 		envMap, readErr := godotenv.Unmarshal(envStr)
 		if readErr != nil {
-			log.Println("Error: Cannot read contents of .env file")
+			log.Fatalln("Error: Cannot read contents of .env file")
 		}
 
-		config = &Config {
-			AppConfig: AppConfig {
-				Env: getKey(envMap,"APP_ENV", defaultConfig.AppConfig.Env),
-				Debug: getKeyAsBool(envMap,"APP_DEBUG", defaultConfig.AppConfig.Debug),
-				Addr: getKey(envMap,"APP_ADDR", defaultConfig.AppConfig.Addr),
+		config = &Config{
+			AppConfig: AppConfig{
+				Env:   getKey(envMap, "APP_ENV", defaultConfig.AppConfig.Env),
+				Debug: getKeyAsBool(envMap, "APP_DEBUG", defaultConfig.AppConfig.Debug),
+				Addr:  getKey(envMap, "APP_ADDR", defaultConfig.AppConfig.Addr),
 			},
-			DBConfig: DBConfig {
-				Driver: getKey(envMap,"DB_DRIVER", defaultConfig.DBConfig.Driver),
-				Name: getKey(envMap,"DB_NAME", defaultConfig.DBConfig.Name),
-				User: getKey(envMap,"DB_USER", defaultConfig.DBConfig.User),
-				Pass: getKey(envMap,"DB_PASS", defaultConfig.DBConfig.Pass),
-				Host: getKey(envMap,"DB_HOST", defaultConfig.DBConfig.Host),
-				Port: getKey(envMap,"DB_PORT", defaultConfig.DBConfig.Port),
+			DBConfig: DBConfig{
+				Driver: getKey(envMap, "DB_DRIVER", defaultConfig.DBConfig.Driver),
+				Name:   getKey(envMap, "DB_NAME", defaultConfig.DBConfig.Name),
+				User:   getKey(envMap, "DB_USER", defaultConfig.DBConfig.User),
+				Pass:   getKey(envMap, "DB_PASS", defaultConfig.DBConfig.Pass),
+				Host:   getKey(envMap, "DB_HOST", defaultConfig.DBConfig.Host),
+				Port:   getKey(envMap, "DB_PORT", defaultConfig.DBConfig.Port),
 			},
 		}
 	})
@@ -94,14 +95,14 @@ func getKeyAsBool(env map[string]string, key string, defaultValue bool) bool {
 	valueStr := getKey(env, key, "")
 	if val, err := strconv.ParseBool(valueStr); err == nil {
 		return val
-    }
-    return defaultValue
+	}
+	return defaultValue
 }
 
 func getKeyAsInt(env map[string]string, key string, defaultValue int) int {
 	valueStr := getKey(env, key, "")
 	if value, err := strconv.Atoi(valueStr); err == nil {
 		return value
-   	}
+	}
 	return defaultValue
 }
