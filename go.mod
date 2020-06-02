796d8d8de50edf7caf648fb0201d6316f0f453eb
Sergio Martin <smartinsantos@gmail.com>
2020-06-01 21:39:24 -0500

    saving progress

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module sherman
 go 1.14
 
 require (
+	github.com/DATA-DOG/go-sqlmock v1.4.1
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/go-sql-driver/mysql v1.5.0
@@ -12,9 +13,8 @@ require (
 	github.com/labstack/echo/v4 v4.1.16
 	github.com/rs/zerolog v1.18.0
 	github.com/sarulabs/di v2.0.0+incompatible
-	github.com/stretchr/testify v1.5.1 // indirect
+	github.com/stretchr/testify v1.6.0
 	golang.org/x/crypto v0.0.0-20200510223506-06a226fb4e37
 	golang.org/x/sys v0.0.0-20200523222454-059865788121 // indirect
 	gopkg.in/check.v1 v1.0.0-20180628173108-788fd7840127 // indirect
-	gopkg.in/yaml.v2 v2.3.0 // indirect
 )
---
+++ src/test/repository/security_token_repository_test.go
@@ -0,0 +1,33 @@
+package test
+
+import (
+	"github.com/DATA-DOG/go-sqlmock"
+	"github.com/google/uuid"
+	"github.com/stretchr/testify/assert"
+	"sherman/src/domain/auth"
+	"sherman/src/repository/mysqlds"
+	"testing"
+	"time"
+)
+
+func TestCreateOrUpdateToken(t *testing.T) {
+	st := &auth.SecurityToken{
+		ID: uuid.New().String(),
+		UserID: "some-user-id",
+		Token: "some-user-token",
+		Type: "some-token-type",
+		CreatedAt: time.Now(),
+		UpdatedAt: time.Now(),
+	}
+
+	db, mock, err := sqlmock.New()
+	if err != nil {
+		t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
+	}
+
+	mock.ExpectExec("INSERT security_tokens SET").WithArgs(st.ID, st.UserID, st.Token, st.Type, st.CreatedAt, st.UpdatedAt)
+
+	var securityTokenRepository auth.SecurityTokenRepository = &mysqlds.SecurityTokenRepository{ DB: db }
+	err = securityTokenRepository.CreateOrUpdateToken(st)
+	assert.NoError(t, err)
+}
