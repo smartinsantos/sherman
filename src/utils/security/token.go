9c06ca1aeecf54fb2a293db283dbbed8ab12874c
Sergio Martin <smartinsantos@gmail.com>
2020-06-05 08:25:02 -0500

    saving progress

--- src/utils/security/token.go
+++ src/utils/security/token.go
@@ -11,8 +11,7 @@ import (
 )
 
 // GenToken generates a jwt.token
-func GenToken(userID, tokenType string, exp int64) (string, error)
-{
+func GenToken(userID, tokenType string, exp int64) (string, error) {
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
 		"user_id": userID,
 		"type":    tokenType,
